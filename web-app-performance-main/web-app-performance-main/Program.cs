3597dea9fc586d973ab09bac5dccb6202b5b2c6f
Fe_Andrd <160950998+FefaBotareli@users.noreply.github.com>
2024-10-24 20:06:19 -0300

    Add files via upload
    Final da entrega da atividade 5

---
+++ web-app-performance-main/web-app-performance-main/Program.cs
@@ -0,0 +1,44 @@
+using web_app_repository;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// 1. Defina uma política de CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigins",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:3000") // Substitua pelo endereço do seu frontend
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Configurar injeção de dependência
+//Vocês vão esquecer de fazer isso e dará erro! 
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("AllowSpecificOrigins");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
