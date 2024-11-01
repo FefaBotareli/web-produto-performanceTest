d8a2c2388a59fb1263dbfec148a2c8793e075a8c
Fe_Andrd <160950998+FefaBotareli@users.noreply.github.com>
2024-10-31 21:41:48 -0300

    Add files via upload
    Checkpoint 3

---
+++ web-app-performance-main/web-app-performance-main/web-app-performance/ConsumidorMensageira.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace web_app_performance
+{
+    public class ConsumidorMensageira
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            const string fila = "produto_cadastrado";
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(fila,
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var mensagem = Encoding.UTF8.GetString(body);
+                    Console.WriteLine($"Estoque atualizado: {mensagem}");
+                };
+
+                channel.BasicConsume(fila,
+                                     autoAck: true, 
+                                     consumer: consumer);
+
+                Console.WriteLine("Aguardando mensagens...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
+    
+
---
+++ web-app-performance-main/web-app-performance-main/web-app-performance/FilaProdutoCadastrado.Mensageira.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+using web_app_domain;
+
+namespace web_app_performance
+{
+    public class FilaProdutoCadastrado
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "produto_cadastrado",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+
+                Produto produto = new Produto
+                {
+                    Id = 123,
+                    Nome = "Teclado Gamer",
+                    QuantidadeEstoque = 10
+                };
+
+                // Serializando o objeto Produto para JSON
+                string mensagem = JsonConvert.SerializeObject(produto);
+                var body = Encoding.UTF8.GetBytes(mensagem);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "produto_cadastrado",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine($" {mensagem}");
+            }
+        }
+
+    }
+}
